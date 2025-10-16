40d63ef2ac22719cec923bc56116c35b5a5b3025
Pierre Verstegen <pierrev21@gmail.com>
2025-10-16 09:52:49 +0200

    Added urls for Viewsets

---
+++ characters/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from characters.views import NPCViewset, PlayerViewset, AttributeViewset, AbilitiesViewset, EffectViewset
+
+router = DefaultRouter()    
+
+
+
+router.register(r'npcs', NPCViewset, basename='npc')
+router.register(r'players', PlayerViewset, basename='player')
+router.register(r'attributes', AttributeViewset, basename='attribute')
+router.register(r'abilities', AbilitiesViewset, basename='ability')
+router.register(r'effects', EffectViewset, basename='effect')
+
+urlpatterns = [
+    path('', include(router.urls))
+    
+]
