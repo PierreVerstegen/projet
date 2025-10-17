56a465e21de2bb338815b6f41b69d4be1173e50b
Pierre Verstegen <pierrev21@gmail.com>
2025-10-17 12:34:25 +0200

    Added games app

--- games/views.py
+++ games/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
 
-# Create your views here.
+from games.serializers import GameSerializer
+
+from .models import Game
+
+class GameViewset(viewsets.ModelViewSet) :
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    permission_classes = [IsAuthenticated]
