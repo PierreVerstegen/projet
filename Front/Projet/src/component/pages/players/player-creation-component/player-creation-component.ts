a783793d981eba2403077b1d150745bac677ea4e
Pierre Verstegen <pierrev21@gmail.com>
2025-11-04 12:05:02 +0100

    created base components

---
+++ Front/Projet/src/component/pages/players/player-creation-component/player-creation-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-creation-component',
+  imports: [],
+  templateUrl: './player-creation-component.html',
+  styleUrl: './player-creation-component.css',
+})
+export class PlayerCreationComponent {
+
+}
